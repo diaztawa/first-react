b002c110fad15cc892214ac0d6208f43c6bf8154
Diaz Tawa <diaztawa@gmail.com>
2022-10-31 15:55:27 +0800

    commit-2, parsinta-2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React, { Component } from "react"
+import axios from 'axios'
 
 class App extends Component{
+  constructor(){
+    super()
+
+    this.state = {
+      user: []
+    }
+  }
+
+  getUser = async () => {
+    let response = await axios.get('https://jsonplaceholder.typicode.com/users/1')
+    this.setState({
+      user: response.data
+    })
+  }
+
+  componentDidMount(){
+    this.getUser()
+  }
+
+
   render(){
+    const user = this.state.user
     return (
       <div>
-        <div>Tes masbro</div>
+        <div>Nama saya adalah: {user.name}</div>
+        <div>Username saya adalah: {user.username}</div>
       </div>
     )
   }
 }
 
-export default App
+export default App;
